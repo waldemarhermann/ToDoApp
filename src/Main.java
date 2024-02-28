43e2994deee49f0fa875672f525fd14186e8018a
waldemarhermann <hermann.waldemar1995@gmail.com>
2024-02-28 10:10:39 +0100

    First commit. Add ToDO-, TimedToDo-, ToDoManager-class

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,15 @@
+import javax.swing.*;
 import java.time.LocalDateTime;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ToDoManager toDoManager = new ToDoManager("todoapp_savefile.txt");
 
-        ToDo toDo1 = new ToDo("Erstes", "1.", false);
-        ToDo toDo2 = new ToDo("Zweites ", "2.", false);
-        ToDo toDo3 = new TimedToDo("ErstesT", "1.", false, LocalDateTime.now().plusMinutes(3));
-        ToDo toDo4 = new TimedToDo("ZweitesT", "2.", false, LocalDateTime.now().minusMinutes(3));
+        toDoManager.loadToDos();
 
-        ToDoManager toDoManager = new ToDoManager();
-
-        toDoManager.add(toDo1);
-        toDoManager.add(toDo2);
-        toDoManager.add(toDo3);
-        toDoManager.add(toDo4);
-
-        System.out.println(toDoManager.getNormalToDos());
-        System.out.println(toDoManager.getTimedToDos());
-
-        toDoManager.removeExpiredToDos();
-
-        System.out.println(toDoManager.getTimedToDos());
+        new ToDoWindow(toDoManager);
 
     }
 
--- src/ToDoManager.java
+++ src/ToDoManager.java
@@ -1,11 +1,22 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Time;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class ToDoManager {
 
     // liste todos
-    ArrayList<ToDo> toDos = new ArrayList<>();
+    private ArrayList<ToDo> toDos = new ArrayList<>();
+
+    private String path;
+
+    public ToDoManager(String path) {
+        this.path = path;
+    }
 
     // add
     public void add(ToDo addToDo) {
@@ -50,6 +61,10 @@ public class ToDoManager {
         return normalToDos;
     }
 
+    public ArrayList<ToDo> getToDos() {
+        return this.toDos;
+    }
+
 
     // entferne abgelaufene
     public void removeExpiredToDos() {
@@ -63,4 +78,67 @@ public class ToDoManager {
             }
         }
     }
+
+    public void saveToDos() {
+        File file = new File(this.path);
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fileWriter = new FileWriter(file);
+            for (int i = 0; i < toDos.size(); i++) {
+                ToDo toDo = toDos.get(i);
+
+                String saveString = toDo.getTitel() + "_" + toDo.getBeschreibung() + "_" + toDo.getErledigt();
+                if (toDo instanceof TimedToDo) {
+                    saveString += "_" + ((TimedToDo) toDo).getEndet();
+                }
+
+                fileWriter.write(saveString + "\n");
+            }
+
+            fileWriter.close();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    public void loadToDos()       {
+        File file = new File(this.path);
+
+        if (!file.exists()) return;
+
+        try {
+            FileReader fileReader = new FileReader(file);
+
+            String fullString = "";
+
+            int currentChar = 0;
+            while ((currentChar = fileReader.read()) != -1) {
+                fullString += (char) currentChar;
+            }
+
+            fileReader.close();
+
+            String[] lines = fullString.split("\n");
+            String data;
+            for (int i = 0; i < lines.length; i++) {
+               String line = lines[i];
+               String[] components = line.split("_");
+
+               if (components.length == 3) {
+                   toDos.add(new ToDo(components[0], components[1], Boolean.valueOf(components[2])));
+               } else if (components.length == 4) {
+                    toDos.add(new TimedToDo(components[0], components[1], Boolean.valueOf(components[2]), LocalDateTime.parse(components[3])));
+               }
+            }
+
+        } catch (Exception eoException) {
+            eoException.printStackTrace();
+        }
+    }
 }
+
+
